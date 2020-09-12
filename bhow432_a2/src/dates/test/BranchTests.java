f783065866df0e62328bcf3fb64e5ec57289fd6b
Ben Howard <62419637+ben-howard-55@users.noreply.github.com>
2020-09-12 15:11:21 +1200

    maximised statement and conditonal coverage

--- bhow432_a2/src/dates/test/BranchTests.java
+++ bhow432_a2/src/dates/test/BranchTests.java
@@ -64,12 +64,91 @@ public class BranchTests {
 	
 	/**
 	 * Year is a multiple of 4 (is a leap year)
-	 * and is 2nd Month, but has valid days.
+	 * and is 2nd Month with valid days 
+	 * and day position is not greater than 0.
 	 */
 	@Test
 	public void testYearMultipleOf4and2MonthValid() {
 			Day day = dates.Dates.dayOfWeek(2004, 2, 20);
 			assertEquals(Day.Friday, day);
+	}
+	
+	/**
+	 * Year is a multiple of 4 (is a leap year)
+	 * and is 8nd Month with valid days
+	 * and day position is greater than 0.
+	 */
+	@Test
+	public void testYearMultipleOf4and8Month() {
+			Day day = dates.Dates.dayOfWeek(2004, 8, 20);
+			assertEquals(Day.Friday, day);
+	}
+	
+	/**
+	 * Year is a not a leap year and is 7th Month,
+	 * with valid number of days.
+	 */
+	@Test
+	public void testNormalYearWithValidDays() {
+			Day day = dates.Dates.dayOfWeek(2007, 7, 7);
+			assertEquals(Day.Saturday, day);
+	}
+	
+	/**
+	 * Year is a not a leap year and is 5th Month,
+	 * with invalid number of days.
+	 */
+	@Test
+	public void testNormalYearAndInvalidNumberOfDays() {
+		try {
+			dates.Dates.dayOfWeek(2003, 5, 32);
+			fail("Should have failed");
+		} catch (IllegalArgumentException e) {
+			assertEquals("Invalid date: year=" + 2003 + ", month=" + 5 + ", day=" + 32, e.getMessage());
+		}
+	}
+	
+	/**
+	 * Year is a not a leap year and
+	 * month which is greater than 12 (invalid).
+	 */
+	@Test
+	public void testNormalYearAndInvalidMonthGt() {
+		try {
+			dates.Dates.dayOfWeek(2003, 13, 5);
+			fail("Should have failed");
+		} catch (IllegalArgumentException e) {
+			assertEquals("Invalid date: year=" + 2003 + ", month=" + 13 + ", day=" + 5, e.getMessage());
+		}
+	}
+	
+	/**
+	 * Year is a not a leap year and
+	 * month which is less than 1 (invalid).
+	 */
+	@Test
+	public void testNormalYearAndInvalidMonthLeq() {
+		try {
+			dates.Dates.dayOfWeek(2003, 0, 5);
+			fail("Should have failed");
+		} catch (IllegalArgumentException e) {
+			assertEquals("Invalid date: year=" + 2003 + ", month=" + 0 + ", day=" + 5, e.getMessage());
+		}
+	}
+	
+	/**
+	 * Year is a not a leap year and
+	 * month which is valid and
+	 * day which is less than 1 (invalid).
+	 */
+	@Test
+	public void testNormalYearAndInvalidDayLeq() {
+		try {
+			dates.Dates.dayOfWeek(2003, 2, -1);
+			fail("Should have failed");
+		} catch (IllegalArgumentException e) {
+			assertEquals("Invalid date: year=" + 2003 + ", month=" + 2 + ", day=" + -1, e.getMessage());
+		}
 	}
 	
 
