d429caea5cf81005f6d11d637bb9b3d9c2c54d33
Ben Howard <62419637+ben-howard-55@users.noreply.github.com>
2020-09-12 17:21:28 +1200

    found du-path to include for DataflowTest

--- bhow432_a2/src/dates/test/DataflowTest.java
+++ bhow432_a2/src/dates/test/DataflowTest.java
@@ -1,7 +1,12 @@
 package dates.test;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.rules.Timeout;
+
+import dates.Dates.Day;
 
 /**
  * SOFTENG 254 Assignment 2 DataflowTest
@@ -9,8 +14,32 @@ import org.junit.rules.Timeout;
  * Author: Benjamin James Howard, bhow432
  **/
 public class DataflowTest {
+	/**
+	 * This du-path has not been tested by other tests as none
+	 * of the branch tests test the case when position is equal to 0.
+	 * 
+	 * This only happens when both the anchor day is Sunday and 
+	 * the day is a doomsday or when the ordinality of the anchor day 
+	 * and the days until doomsday are equal in magnitude 
+	 * and days until doomsday is negative (before doomsday date).
+	 */
 
 	@Rule
 	public Timeout globalTimeout = Timeout.millis(100);
+	
+	/**
+	 * Testing the du-path of int position,
+	 * defined on line 102 and used on line 111.
+	 * du-path of; 102,103,105,107,111 (excluding { and }).
+	 * 
+
+	 */
+	@Test
+	public void testPositionDuPath() {
+		Day day = dates.Dates.dayOfWeek(2100,10,10);
+		assertEquals(Day.Sunday, day);
+	}
+	
+	
 
 }
